c179386b0625f3ea815c09d39d3ef980bb42e6b9
maria-mukhanova <120710556+maria-mukhanova@users.noreply.github.com>
2022-12-19 10:07:20 +0200

    Update HelloJdk.java

--- HelloJdk.java
+++ HelloJdk.java
@@ -2,6 +2,6 @@ public class HelloJdk {
 
     public static void main(String[] args) {
 
-            System.out.println("Ура! Этот код работает!");
+            System.out.println("This code works!");
     }
 }
